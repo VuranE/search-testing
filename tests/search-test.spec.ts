b12122e86075cb64bab875f7adec0afebb8e1035
Elma Vuran <elma.vuran@fer.unizg.hr>
2025-10-02 15:57:43 +0200

    Final commit

--- tests/search-test.spec.ts
+++ tests/search-test.spec.ts
@@ -6,72 +6,76 @@ const data = [
         url: 'https://www.njuskalo.hr/',
         search: 'Audi',
         category: 'Audi',
-        yearFrom: '2015',
-        yearTo: '2019',
-        distanceFrom: '0',
-        distanceTo: '200000'
+        yearManufacturedMin: 2015,
+        yearManufacturedMax: 2019,
+        mileageMax: 200000
     },
     {
         name: 'Bolha',
         url: 'https://www.bolha.com/',
         search: 'BMW',
         category: 'BMW',
-        yearFrom: '2018',
-        yearTo: '2022',
-        distanceFrom: '0',
-        distanceTo: '100000'
+        yearManufacturedMin: 2018,
+        yearManufacturedMax: 2022,
+        mileageMax: 100000
     }
 ]
 
 
 for(const site of data){
-    test(`search-test: ${site.name}`, async ({ page }) => {
-        await page.goto(site.url);
+  test(`search-test: ${site.name}`, async ({ page }) => {
+    await page.goto(site.url);
+    const agreeButton = page.locator('#didomi-notice-agree-button');
 
+    if (await agreeButton.isVisible()) {
+      await agreeButton.click();
+    }
+
+    const inputField = page.locator('#keywords');
+    await inputField.fill(site.search);
         
-        const agreeButton = page.locator('#didomi-notice-agree-button');
+    await page.getByRole('button', { name: 'Kategorije' }).click();
+    await page.getByRole('option', { name: site.category, exact: true }).locator('span').click();
         
-        if (await agreeButton.isVisible({ timeout: 5000 })) {
-            await agreeButton.click();
-        }
+    await page.locator('[id="yearManufactured[min]"]').selectOption(site.yearManufacturedMax.toString());
+    await page.locator('[id="yearManufactured[max]"]').selectOption(site.yearManufacturedMax.toString());        
+    await page.locator('input[name="mileage[max]"]').fill(site.mileageMax.toString());
 
-        const inputField = page.locator('#keywords');
+    const submitButton = page.locator('#submitButton');
+    await submitButton.click();
 
+    const items = page.locator('.EntityList-items li');
+    const noResults = page.locator('div.brdr_top.ad_item');
+
+    const count = await items.count();
         
+    if(count == 0){
+        expect(noResults).toBeVisible;
+    } else {
+      expect(count).toBeGreaterThan(0);
+      const descriptions = await page.locator('.entity-description-main').allTextContents();
+               
+      for (const description of descriptions) { 
+        const lines = description.trim().split('\n').map(l => l.trim()).filter(l => l.length > 0);
 
-        await inputField.waitFor();
-        await inputField.fill(site.search);
+        if (lines.length < 3) continue; //not a valid ad -> skip
+
+        // mileage
+        const mileageLine = lines[0];
+        const mileageParts = mileageLine.split(',');
         
-        const categoryButton = page.getByRole('button', { name: 'Kategorije' });
-        await categoryButton.click();
+        const mileageStr = mileageParts[1].replace('km', '').trim();
+        const mileage = parseInt(mileageStr, 10);
 
-            
-        const categoryOption = page.locator('li[role="option"]', { hasText: new RegExp(`^${site.category}$`) });
-        await categoryOption.click();
-
-
-        const filterButton = page.locator('li:has-text("Filtri"), li:has-text("Filteri")');
-        if(await filterButton.isVisible()){
-           await filterButton.click();
-        }
+        // year
+        const yearLine = lines[1];
+        const yearStr = yearLine.split(':')[1].replace('.', '').trim();
+        const year = parseInt(yearStr, 10);
         
-        const minManufacturedYear = page.locator("#yearManufactured\\[min\\]");
-    
-        await minManufacturedYear.selectOption(site.yearFrom);
-
-        const maxManufacturedYear = page.locator("#yearManufactured\\[max\\]");
-        await maxManufacturedYear.selectOption(site.yearTo);
-
-        const mileageMin = page.locator("#mileage\\[max\\]");
-        await mileageMin.fill(site.distanceTo);
-
-        const submitButton = page.locator('#submitButton');
-        await submitButton.click();
-
-       const items = page.locator('.EntityList-items li');
-       const count = await items.count();
-        await expect(count).toBeGreaterThan(0);
-
-    });
-
+        expect(mileage).toBeLessThanOrEqual(site.mileageMax);
+        expect(year).toBeGreaterThanOrEqual(site.yearManufacturedMax);
+        expect(year).toBeLessThanOrEqual(site.yearManufacturedMax);
+      }
+    }
+  });
 }
